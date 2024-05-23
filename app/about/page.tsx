9282f97e8c8e32cb4249fb2f806a56250451a7bb
aftelnes <aftelnes@gmail.com>
2024-05-23 19:25:35 +0300

    add mobile adaptive in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,29 +14,30 @@ const AboutUs = () => {
     <div className={pageStyle.pageStyle}>
       <hr className={pageStyle.pageHr} />
       <div
-        className="relative element-animation shadow-xl shadow-grey-300 container mb-10 m-auto
+        className="relative flex element-animation shadow-xl shadow-grey-300 mb-10 m-auto container mt-5
+        h-[200px] justify-center
       xl:h-[600px]
       lg:h-[450px]
       md:h-[400px]"
       >
         <img
           src={main_img.src}
-          className="absolute shadow-xl
+          className="absolute shadow-xl h-[200px] container
           xl:h-[600px] xl:w-full 
           lg:h-[450px] lg:w-full 
           md:h-[400px] md:w-full"
         />
         <div
-          className="absolute w-full bg-black opacity-45
+          className="absolute w-full bg-black opacity-45 h-[200px] container
         xl:h-[600px]
         lg:h-[450px]
         md:h-[400px]"
         ></div>
         <p
-          className="absolute text-white mt-20 
-        xl:text-3xl xl:w-[900px] xl:ml-[150px]
-        lg:text-2xl lg:w-[800px] lg:ml-[50px]
-        md:text-xl md:w-[650px] md:ml-[50px]
+          className="absolute text-white mt-2 container w-10/12 text-sm
+        xl:text-3xl xl:w-[900px] xl:mt-20 
+        lg:text-2xl lg:w-[800px] lg:mt-16 
+        md:text-xl md:w-[650px] md:mt-14
         "
         >
           Мы уже много лет занимаемся строительством бассейнов под ключ, а так
